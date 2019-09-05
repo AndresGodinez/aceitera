73ec627d208c10a467fde331bd87770cbc404c8b
AGodinez <ing.a.godinez@gmail.com>
2019-09-04 19:30:35 -0500

    Add ground test

---
+++ tests/Feature/GroundTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Municipality;
+use App\PalmFarmer;
+use App\State;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use function factory;
+
+class GroundTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /** @test */
+    public function user_can_add_ground_to_palm_farmer()
+    {
+        $user = $this->getDefaultUser();
+
+
+        $this->seed('StatesAndMunicipalitiesSeeder');
+
+        $palmFarmer = factory(PalmFarmer::class)->create();
+
+        $state = State::find(5);
+
+        $municipalities = Municipality::where('state_id', $state->id)->get();
+
+        $municipality = $municipalities->random();
+
+        $latitude = $this->faker->latitude(-90, 90);
+        $longitude = $this->faker->longitude(-90, 90);
+
+        $response = $this->actingAs($user)->post('/ground', [
+            'palmFarmerId' => $palmFarmer->id,
+            'location' => $municipality->name,
+            'municipality_id' => $municipality->id,
+            'state_id' => $state->id,
+            'latitude' => $latitude,
+            'longitude' => $longitude
+        ]);
+
+        $this->assertDatabaseHas('grounds', [
+            'palmFarmerId' => $palmFarmer->id,
+            'location' => $municipality->name,
+            'municipality_id' => $municipality->id,
+            'state_id' => $state->id,
+            'latitude' => $latitude,
+            'longitude' => $longitude
+        ]);
+
+        $response->assertRedirect('grounds');
+    }
+}
